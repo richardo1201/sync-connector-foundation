7fc6fae1456adcaca31acf5ccb0f89f0105557ee
richardokusuma98 <richardokusuma@yahoo.com>
2020-04-12 08:25:51 +0700

    Init commit

---
+++ src/main/java/com/dft/syncconnectorfoundation/SyncConnectorFoundationApplication.java
@@ -0,0 +1,13 @@
+package com.dft.syncconnectorfoundation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncConnectorFoundationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncConnectorFoundationApplication.class, args);
+	}
+
+}
